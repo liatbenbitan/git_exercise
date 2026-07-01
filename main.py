39694152918af5623e5a60bfc65c2900efa389fa
liatbenbitan <liatbenbitan@gmail.com>
2026-07-01 20:40:41 +0300

    Updating the model name to Linear Regression

--- main.py
+++ main.py
@@ -2,4 +2,8 @@
 # New Python file!
 # New comment MISTAKE
 # This is new comment to check commit
+def get_model_name():
+    return "Linear Regression Model"
+
+print("Active Model:", get_model_name())
 
